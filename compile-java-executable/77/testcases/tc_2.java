1caec8dd25567b71ec817efec6b9e8d0e7d9a919
Uvaish Sindhi <uvaish.sindhi+1@testgrid.io>
2026-07-23 09:40:54 -0700

    Restored Version from Git Commit - Auto Commit

--- compile-java-executable/77/testcases/tc_2.java
+++ compile-java-executable/77/testcases/tc_2.java
@@ -29,6 +29,11 @@ public class tc_2 {
 		// [DISABLED] tg.testFunction("call_fun_1", new Object[]{});
 		// [DISABLED] tg_String var_return = "null";
 		tg.testFunction("presskey_fun", new Object[]{});
+		tg.wait(5);
+		// [DISABLED] tg.testFunction("screenshot", new Object[]{});
+		// [DISABLED] tg.wait(2);
+		// [DISABLED] tg.printLogs(var_return);
+		tg.wait(5);
 		tg.close();
 	}
 }
