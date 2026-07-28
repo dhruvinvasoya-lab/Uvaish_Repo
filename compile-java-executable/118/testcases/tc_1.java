a7a3133b889c7b25884e78db144033faace0295c
Uvaish Sindhi <uvaish.sindhi+5@testgrid.io>
2026-07-28 14:03:35 +0000

    commit tc1

--- compile-java-executable/118/testcases/tc_1.java
+++ compile-java-executable/118/testcases/tc_1.java
@@ -26,6 +26,17 @@ public class tc_1 {
 	public void tc_1() {
 		tg.openBrowser();
 		tg.wait(5);
+		// [DISABLED] tg.wait("ele_firstname343", ComparisonType.IS_VISIBLE);
+		// [DISABLED] tg.click("ele_firstname343", 1);
+		// [DISABLED] tg.wait("ele_firstname343", ComparisonType.IS_VISIBLE);
+		tg_String var_name = "";
+		var_name = tg.saveToVariable("#TGITVAR.name", var_name);
+		tg.printLogs(var_name);
+		// [DISABLED] tg.type("ele_firstname343", "uvaish");
+		// [DISABLED] tg.wait("ele_lastname015", ComparisonType.IS_VISIBLE);
+		// [DISABLED] tg.click("ele_lastname015", 1);
+		// [DISABLED] tg.wait("ele_lastname015", ComparisonType.IS_VISIBLE);
+		// [DISABLED] tg.type("ele_lastname015", "si");
 		tg.close();
 	}
 }
